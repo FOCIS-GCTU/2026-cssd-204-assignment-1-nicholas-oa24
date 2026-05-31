f09a6bd46679ee788a5e5c0192751040a3a45df3
nicholas-oa24 <2425403107@live.gctu.edu.gh>
2026-05-31 09:42:27 +0000

    Update initials.py

--- initials.py
+++ initials.py
@@ -1,7 +1,33 @@
-
+# File: initials.py
+# Description: Print my initials in CAPS letters.
+# Assignment Number: 1
+#
+# Name: Nicholas Sefa Kumah
+# STUDENT ID: 2425403107
+# Email: 2425403107@livw.gctu.edu.gh
+# Grader: Augustus Buckman
+#
+# On my honor, Nicholas Sefa Kumah, this programming assignment is my own work
+# and I have not provided this code to any other student.
 def main():
-  pass #code goes here
+    # Print my  initials
 
+    print()
+    print("...NSK")
+    print()
+
+    print("...NN........NN........SSSSSSSSSSSS........KK........KK.....")
+    print("...NNN.......NN........SSSSSSSSSSSS........KKK......KK......")
+    print("...NNNN......NN........SSSS................KKKK....KK.......")
+    print("...NN.NN.....NN........SSSS................KKKK...KK........")
+    print("...NN..NN....NN.........SSSSSSS............KKKKKK.K.........")
+    print("...NN...NN...NN...........SSSSSS...........KKKKKKKK.........")
+    print("...NN....NN..NN................SSSS........KKKKKK.KK........")
+    print("...NN.....NN.NN................SSSS........KKKK....KK.......")
+    print("...NN......NNNN...**...SSSSSSSSSSSS...**...KKK......KK....**")
+    print("...NN.......NNN...**...SSSSSSSSSSSS...**...KK........KK...**")
+
+    print()
 
 
 main()
